03e93b8aff79177d7b1ea52b356ed1136a3503f0
juhwanHeo <juhwanheo.dev@gmail.com>
2022-02-16 19:53:48 +0900

    22.02.16 - 1 (desktop)
    # DO
    1, 캘린더 UI 구성
    2, 캘린더 prev, next 활성화
    3, 캘린더 일정 클릭 시 일정 정보 출력

--- LeagueMatchInfo/src/components/MyCalendar.vue
+++ LeagueMatchInfo/src/components/MyCalendar.vue
@@ -1,34 +1,148 @@
 <template>
   <div>
     <v-sheet width="100%">
-      <v-btn
-        icon
-        class="ma-2"
-        @click="$refs.calendar.prev()"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
+      <v-toolbar
+          flat
+        >
+        <v-btn
+          icon
+          fab
+          text
+          small
+          class="ma-2"
+          @click="prev"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          fab
+          text
+          small
+          class="ma-2"
+          @click="next"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
 
-      <v-btn
-        icon
-        class="ma-2"
-        @click="$refs.calendar.next()"
-      >
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
+        <v-toolbar-title v-if="$refs.calendar">
+          {{ getTitle }}
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+          <v-menu
+            bottom
+            right
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                outlined
+                color="grey darken-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <!--
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+              -->
+            </v-list>
+          </v-menu>
+      </v-toolbar>
     </v-sheet>
     <v-sheet width="100%" height="600">
 
       <v-calendar
         ref="calendar"
+        v-model="focus"
         :event-overlap-mode="'stack'"
         :event-overlap-threshold="30"
+        :events="events"
+        :type="type"
+        @click:event="showEvent"
+        @click:more="viewDay"
+        @click:date="viewDay"
+        :loading="true"
 
       ></v-calendar>
+
+      <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <div>
+                <v-icon>mdi-information</v-icon>
+                <span class="event-info">리그: {{selectedEvent.leag_nm}}</span>
+              </div>
+              <div>
+                <v-icon>mdi-information</v-icon>
+                <span class="event-info">종목: {{selectedEvent.game_nm}}</span>
+              </div>
+              <div>
+                <v-icon>mdi-map-marker</v-icon>
+                <span class="event-info">경기장: {{selectedEvent.game_arena_nm}}</span>
+              </div>
+              <div>
+                <v-icon>mdi-clock-time-eight</v-icon>
+                <span class="event-info">시간: {{selectedEvent.time}}</span>
+              </div>
+              <div>
+                <v-icon>mdi-information</v-icon>
+                <span class="event-info">진행상황: {{selectedEvent.process_nm}}</span>
+              </div>
+
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
     </v-sheet>
   </div>
 </template>
 <style>
+.event-info {
+  color: black;
+  font-weight: bold;
+
+
+}
 .my-chart-view {
   /* display: inline !important; */
   padding: 10px;
@@ -44,29 +158,69 @@
   export default {
     name: 'MyCalendar',
     props:{
-      
+      events: {
+        type: Array,
+        default: []
+      }
     },
     data() {
       return {
-
+        focus: '',
+        type: 'month',
+        typeToLabel: {
+          month: 'Month',
+          week: 'Week',
+          day: 'Day',
+          '4day': '4 Days',
+        },
+        selectedEvent: {},
+        selectedElement: null,
+        selectedOpen: false,
       }
     },
     async mounted() {
       // 마운트 된 두 ref에 접근할 수 있으므로 마운트 뒤 차트를 그린다.
       // this.drawChart(this.type);
+      this.$refs.calendar.checkChange()
+
+
     },
     watch: {
-      // selected: function (val) {
-      //   if(val) {
-      //     if (typeof val === 'object')
-      //       this.drawChart(val.type);
 
-      //     else this.drawChart(val);
-
-      //   }
-      // }
+    },
+    computed: {
+      getTitle() {
+        let titleSplit = this.$refs.calendar.title.split('월')
+        return titleSplit[1].trim() + '년 ' + titleSplit[0] + '월'
+      }
     },
     methods: {
+      viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          requestAnimationFrame(() => requestAnimationFrame(() => open()))
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      }
     }
   }
 </script>
--- LeagueMatchInfo/src/views/CalendarViews.vue
+++ LeagueMatchInfo/src/views/CalendarViews.vue
@@ -1,20 +1,30 @@
 <template>
+
   <v-container
     id="dashboard-view"
     fluid
     tag="section"
   >
-    <v-row
-      align="center">
-      <v-col
-        cols="12"
-        sm="12">
+    <material-card
+      :loading="isCalendarLoading"
+      color="accent"
 
-        <my-calendar>
-        </my-calendar>
+      >
+      <v-row
+        align="center">
+        <v-col
+          cols="12"
+          sm="12">
 
-      </v-col>
-    </v-row>
+          <my-calendar
+            :events="events"
+            >
+          </my-calendar>
+
+        </v-col>
+      </v-row>
+    </material-card>
+
 
     </v-container>
 </template>
@@ -39,28 +49,99 @@
     name: 'calendarViews',
     data() {
       return {
+        isCalendarLoading: false,
+        events: [],
       }
     },
     mounted() {
-        // this.init();
-    },
-    watch: {
-        // leagueSelect: function(val) {
-        //     // 통계 데이터 가져오기
-
-        //     this.arenaData = null;
-        //     this.attendData = null;
-        //     this.gameAvgAttendData = null;
-        //     this.startTimeData = null;
-        //     this.getStatistics(val);
-        // }
+        this.init()
     },
     methods: {
       init() {
-          // alert('리그 통계 준비중 입니다. \nPreparing to open the League Statistics. ')
-          this.getLeagues();
+          this.getGameSchedule();
       },
-      
+      getColor(data) {
+        let color
+
+        switch(data) {
+          case '001001':
+            color = 'deep-purple'
+            break
+          case '001002':
+            color = 'green'
+            break
+          case '001003':
+            color = 'blue'
+            break
+          case '001004':
+            color = 'indigo'
+            break
+          case '001005':
+            color = 'red'
+            break
+          case '001006':
+            color = 'cyan'
+            break
+          case '001007':
+            color = 'orange'
+            break
+          case '001008':
+            color = 'grey darken-1'
+            break
+          default:
+            color = 'black'
+        }
+
+        return color
+      },
+      getDetails(data) {
+        // example / not Used
+        let details = ''
+        details += '<span>리그: </span> ' + data.leag_nm + '<br>'
+        details += '<span>종목: </span>' + data.game_nm + '<br>'
+        details += '<span><v-icon>mdi-dots-vertical</v-icon>경기장: </span>' + data.game_arena_nm + '<br>'
+        details += '<span>진행상황: </span>' + data.process_nm+ '<br>'
+
+        return details
+      },
+      async getGameSchedule() {
+          console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+          this.isCalendarLoading = true;
+          await axios.get(`/api/leagues/game-schedules`)
+              .then((result) => {
+                let data =  result.data.data;
+                console.log("getGameSchedule: %o" , data);
+
+                for (let val of data) {
+                  const startDate = new Date(val.start.replaceAll('T', ' '))
+                  const endDate = new Date(val.end.replaceAll('T', ' '))
+
+                  const startTimestamp = new Date(startDate.getTime() - (startDate.getTime() % 900000));
+                  const endTimestamp = new Date(endDate.getTime() - (endDate.getTime() % 900000));
+                  const allDay = startTimestamp - endTimestamp === 0;
+
+                  this.events.push({
+                    details: this.getDetails(val),
+                    color: this.getColor(val.game_cd),
+                    start: startDate,
+                    end: endDate,
+                    time: val.start.replaceAll('T', ' ') + ' ~ ' + val.end.replaceAll('T', ' '),
+                    name: val.matches,
+                    leag_nm: val.leag_nm,
+                    game_nm: val.game_nm,
+                    game_arena_nm: val.game_arena_nm,
+                    process_nm: val.process_nm,
+                    timed: !allDay,
+                  })
+                }
+              })
+              .catch((err) => {
+                  console.log(err);
+              })
+          console.log('this.events: %o', this.events)
+          this.isCalendarLoading = false;
+      },
+
     }
   }
 </script>
